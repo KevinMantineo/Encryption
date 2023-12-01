dec996a8f9b1694eb132d6d0b645c1d259633418
KevinMantineo <mantineokevin@gmail.com>
2023-12-01 13:41:10 +0000

    caricamento classe avvio

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+
+    System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto!");
+  }
+  Matrice m = new Matrice("TPSIT");
+  Vigenere v = new Vigenere(0,12,0,12,m);
+  Vigenere v1 = new Vigenere(0,12,0,12,m);
+  Vigenere v2 = new Vigenere(0,12,0,12,m);
+  Vigenere v3 = new Vigenere(0,12,0,12,m);
+
+  thread t1 = new thread(v);
+  thread t2 = new thread(v1);
+  thread t3 = new thread(v2);
+  thread t4 = new thread(v3);
+  
+}
